296f2fcab7eeaef88d3bc7e47b018281987c3f26
Saulo Vallory <saulo@toki.life>
2024-03-15 02:06:59 -0300

    updates to hyperize-all-letters.js

--- rules/hyperize-all-letters.js
+++ rules/hyperize-all-letters.js
@@ -1,4 +1,11 @@
-import { rule, map, ifDevice, withCondition, withMapper } from 'karabiner.ts'
+import {
+  rule,
+  map,
+  ifDevice,
+  withCondition,
+  withMapper,
+  withModifier,
+} from 'karabiner.ts'
 
 const a = 'a'.charCodeAt(0)
 const z = 'z'.charCodeAt(0)
@@ -9,10 +16,25 @@ for (let i = a; i <= z; i++) {
 }
 
 // rule(), layer(), simlayer(), hyperLayer(), duoLayer()
-export default rule('Hyperize All Letters').manipulators([
-  // You can get the product id from Karabiner
-  // 45890 is K380's product id
-  withCondition(ifDevice({ product_id: 45890 }))(
-    withMapper(letter_keys)((k, i) => map(k).to(k, '⌘⌥⌃⇧')),
-  ),
-])
+export default [
+  rule('a-z are Hyped Keys!').manipulators([
+    // You can get the product id from Karabiner
+    // 45890 is K380's product id
+    withCondition(ifDevice({ product_id: 45890 }))(
+      withMapper(letter_keys)((k, i) => map(k).to(k, '⌘⌥⌃⇧')),
+    ),
+  ]),
+
+  rule('A-Z are Hyped Keys!').manipulators([
+    withCondition(ifDevice({ product_id: 45890 }))(
+      withMapper(letter_keys)((k, i) =>
+        withModifier('caps_lock')(map(k).to(k, '⌘⌥⌃⇧⇪')),
+      ),
+    ),
+    withCondition(ifDevice({ product_id: 45890 }))(
+      withMapper(letter_keys)((k, i) =>
+        withModifier('shift')(map(k).to(k, '⌘⌥⌃⇧⇪')),
+      ),
+    ),
+  ]),
+]
